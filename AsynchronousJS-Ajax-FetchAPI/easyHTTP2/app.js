ab2f7aeec1796a840938a680bcc2db4354af7b01
Rafael Saraiva <Rafael.Mielczarski@ibm.com>
2022-08-01 12:19:48 -0300

    add EasyHttp2 folder

---
+++ AsynchronousJS-Ajax-FetchAPI/easyHTTP2/app.js
@@ -0,0 +1,29 @@
+const http = new EasyHTTP;
+
+// //Get Users
+// const users = http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+//Create user data
+const data = {
+    name: 'Rafael Saraiva',
+    username: 'R_Saraiva',
+    email: 'saraiva_cmr@hotmail.com'
+}
+
+//Create User(POST)
+http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(error));
+
+//Update User(PUT)
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(error));
+
+
+//Delete User(PUT)
+http.delete('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(error));
---
+++ AsynchronousJS-Ajax-FetchAPI/easyHTTP2/easyHTTP2.js
@@ -0,0 +1,68 @@
+/**
+ * EASYHTTP Library
+ * Library for making HTTP Requests
+ * 
+ * @version 2.0.0
+ * @author Rafael Saraiva
+ * @license MIT
+ * 
+ **/
+
+class EasyHTTP {
+    //Make an HTTP GET request
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+    }
+
+    //Make an HTTP POST request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+    }
+
+    //Make an HTTP PUT request
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+    }
+
+    //Make an HTTP DELETE request
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            })
+                .then(response => response.json())
+                .then(data => resolve("Deleted"))
+                .catch(error => reject(error));
+        });
+    }
+}
